94f8e3598b2cfb6cf4c5f88c09aa74f2ba77092b
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2022-03-10 22:06:56 +0530

    Update XOR in array.cpp

--- XOR in array.cpp
+++ XOR in array.cpp
@@ -4,7 +4,7 @@ public:
         int ans = 0;
         int nums[n];
         for(int i=0;i<n;i++){
-            nums[i] = start+2*i;
+            nums[i] = start + 2*i;
             ans = ans^nums[i];
         }
         return ans;
