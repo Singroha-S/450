467861fc06431045365a5a2bc87f8fe903307fbe
Sahil Singroha <59840531+Singroha-S@users.noreply.github.com>
2022-03-20 14:17:19 +0530

    Update Count_1.cpp

--- Count_1.cpp
+++ Count_1.cpp
@@ -4,7 +4,7 @@ public:
         int cnt=0;
         for(int i=31;i>=0;i--){
             int a = (n&1);
-            n=n>>1;
+            n = n>>1;
             if(a==1)
                 cnt++;
         }
